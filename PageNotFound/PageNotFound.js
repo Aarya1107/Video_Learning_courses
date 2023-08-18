0d438b8db772765bbbe504ae3bd94422ee65ed1b
Deepa Arya <126440385+Aarya1107@users.noreply.github.com>
2023-08-18 09:53:25 +0530

    in src

---
+++ PageNotFound/PageNotFound.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Styles from "./PageNotFound.module.css"
+import Header from "../Components/LogOutPage/Header/Header"
+import Footer from "../Components/LogOutPage/Footer/Footer"
+
+export default function PageNotFound() {
+  return (
+    <>
+      <Header />
+      <div className={Styles["Error-fullPage"]}>
+        <div>
+          <p className={Styles["Error-heading"]}>
+            <i>Error 404 </i>
+          </p>
+          <img
+            src="./image/Error404.png"
+            alt=""
+            className={Styles["Error-Image"]}
+          ></img>
+          <div className={Styles["last-Para-div"]}>
+            <p className={Styles["para"]}>
+              We can’t find the page you’re looking for
+            </p>
+            <p>
+              Visit our <a href="/">support page </a> for further assistance.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
